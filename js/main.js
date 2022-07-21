ecc727d5f151eb2a26c0cf4b5f23ad542ce2aa03
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-07-21 23:26:49 +0500

    Add new page contact

--- js/main.js
+++ js/main.js
@@ -18,3 +18,26 @@ elModalCloseButton.addEventListener("click" , function (){
 elModalCloseButton.addEventListener("click",function(){
   elModal.classList.remove("modal-show-active");
 })
+
+
+
+var elModalOpenButton = document.querySelector(".index-modal");
+var elModal = document.querySelector(".modal");
+
+elModalOpenButton.addEventListener("click", function(){
+    elModal.classList.add("modal-show-active");
+})
+
+
+var elModalCloseButton = document.querySelector(".modal-show__close")
+
+elModalCloseButton.addEventListener("click" , function (){
+  // `  // elModalWindnow.classList.add("modal-window-close");
+    elModal.classList.remove("modal-show-active");
+})
+
+
+
+elModalCloseButton.addEventListener("click",function(){
+  elModal.classList.remove("modal-show-active");
+})
